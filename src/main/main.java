15d9bf2b8856e13149c336b020f4cf5917b3d26c
gedubrackkk <152798464+gedubrackkk@users.noreply.github.com>
2024-01-07 22:50:47 +0700

    Update main.java

--- src/main/main.java
+++ src/main/main.java
@@ -1,4 +1,5 @@
 package main;
+import java.sql.^;
 
 public class main {
 
@@ -6,5 +7,16 @@ public class main {
 		// TODO Auto-generated method stub
 
 	}
-
+public class DatabaseConnection {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/RestoManagement", "username", "password");
+            // here RestoManagement is the database name, username is your database username, password is your database password
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
 }
