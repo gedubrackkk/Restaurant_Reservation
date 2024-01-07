4532c05bf69a6b4b09a25b72487a53055cda35eb
gedubrackkk <152798464+gedubrackkk@users.noreply.github.com>
2024-01-07 23:23:52 +0700

    Update SpecialMenu.java

--- src/main/SpecialMenu.java
+++ src/main/SpecialMenu.java
@@ -1,1 +1,18 @@
+public class SpecialMenu extends Menu {
+    private String story; // narasi cerita dari menu
+
+    public SpecialMenu(String Menuid, String Menuname, double price, String location, String story) {
+        super(Menuid, Menuname, price, location); // memanggil konstruktor dari superclass (Menu)
+        this.story = story; // inisialisasi atribut tambahan untuk SpecialMenu
+    }
+
+    // getters and setters untuk story
+    public String getStory() {
+        return story;
+    }
+
+    public void setStory(String story) {
+        this.story = story;
+    }
+}
 
